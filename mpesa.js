80869fec9de84ef557e1586b2051985e5d0a5145
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-11-27 20:50:57 +0300

    fix: replace paymeny to have till number pay to instead of paybill

--- mpesa.js
+++ mpesa.js
@@ -57,7 +57,7 @@ export const stkPush = async ({ phone, amount }) => {
     BusinessShortCode: process.env.DARAJA_SHORTCODE,
     Password: password,
     Timestamp: timestamp,
-    TransactionType: "CustomerPayBillOnline",
+    TransactionType: "CustomerBuyGoodsOnline",
     Amount: amount,
     PartyA: phone,
     PartyB: process.env.DARAJA_SHORTCODE,
